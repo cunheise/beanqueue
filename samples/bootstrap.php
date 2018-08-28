7581820a41115a9b557d3d6e547cb1021f1449bf
Nathan Wu <nathan@joomlacreator.com>
2018-08-28 11:03:42 +0800

    add logger for lib

--- samples/bootstrap.php
+++ samples/bootstrap.php
@@ -9,6 +9,9 @@
 use Beanqueue\Queue;
 use Pheanstalk\Pheanstalk;
 
+if (PHP_SAPI !== 'cli') {
+    exit("cli only!" . PHP_EOL);
+}
 require dirname(__DIR__) . '/vendor/autoload.php';
 $config = require __DIR__ . '/config.php';
 $queue = new Queue(['pheanstalk' => new Pheanstalk($config['host'], $config['port']), 'name' => 'test']);
